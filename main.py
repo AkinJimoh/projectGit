8e205d380aea56caf0bbe68e429766112b239d1f
Akin <akinola.jimoh@digital.homeoffice.gov.uk>
2022-05-20 17:38:36 +0100

    Added python script

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import os
 ENVIRONMENT = os.getenv('TEST_STRING')
 
 print(f"The current environment is {ENVIRONMENT.lower()}")
+
+main = "main.sh"
+replace_with = ENVIRONMENT.lower() + "." + main
+
+with open(replace_with, "r") as f:
+    data = f.read()
+
+with open(main, "w") as f:
+    f.write(data)
